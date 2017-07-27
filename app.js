826adb98cb695577d116458384bc29db01376730
jkwong5 <jkwong5@gmail.com>
2017-07-27 15:44:53 -0700

    adding richtext

--- app.js
+++ app.js
@@ -468,30 +468,30 @@ app.post('/webhook/', (req, res) => {
     }
 
 });
-
-/* For Facebook Validation */
-app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    res.status(403).end();
-  }
-});
+//
+// /* For Facebook Validation */
+// app.get('/webhook', (req, res) => {
+//   if (req.query['hub.mode'] && req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+//     res.status(200).send(req.query['hub.challenge']);
+//   } else {
+//     res.status(403).end();
+//   }
+// });
 
 /* Handling all messenges */
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  if (req.body.object === 'page') {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-        if (event.message && event.message.text) {
-          sendMessage(event);
-        }
-      });
-    });
-    res.status(200).end();
-  }
-});
+// app.post('/webhook', (req, res) => {
+//   console.log(req.body);
+//   if (req.body.object === 'page') {
+//     req.body.entry.forEach((entry) => {
+//       entry.messaging.forEach((event) => {
+//         if (event.message && event.message.text) {
+//           sendMessage(event);
+//         }
+//       });
+//     });
+//     res.status(200).end();
+//   }
+// });
 
 // function sendMessage(event) {
 //   let sender = event.sender.id;
@@ -520,41 +520,41 @@ app.post('/webhook', (req, res) => {
 
 /* GET query from API.ai */
 
-function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
-
-  let apiai = apiaiApp.textRequest(text, {
-    sessionId: 'tabby_cat'
-  });
-
-  apiai.on('response', (response) => {
-    console.log(response);
-    let aiText = response.result.fulfillment.speech;
-
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: FB_PAGE_ACCESS_TOKEN},
-      method: 'POST',
-      json: {
-        recipient: {id: sender},
-        message: {text: aiText}
-      }
-    }, (error, response) => {
-      if (error) {
-          console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error);
-      }
-    });
-  });
-
-  apiai.on('error', (error) => {
-    console.log(error);
-  });
-
-  apiai.end();
-}
+// function sendMessage(event) {
+//   let sender = event.sender.id;
+//   let text = event.message.text;
+//
+//   let apiai = apiaiApp.textRequest(text, {
+//     sessionId:
+//   });
+//
+//   apiai.on('response', (response) => {
+//     console.log(response);
+//     let aiText = response.result.fulfillment.speech;
+//
+//     request({
+//       url: 'https://graph.facebook.com/v2.8/me/messages',
+//       qs: {access_token: FB_PAGE_ACCESS_TOKEN},
+//       method: 'POST',
+//       json: {
+//         recipient: {id: sender},
+//         message: {text: aiText}
+//       }
+//     }, (error, response) => {
+//       if (error) {
+//           console.log('Error sending message: ', error);
+//       } else if (response.body.error) {
+//           console.log('Error: ', response.body.error);
+//       }
+//     });
+//   });
+//
+//   apiai.on('error', (error) => {
+//     console.log(error);
+//   });
+//
+//   apiai.end();
+// }
 
 /* Webhook for API.ai to get response from the 3rd party API */
 app.post('/ai', (req, res) => {
