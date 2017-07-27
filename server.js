354fbe72190feb47b62c9e34c6b84f3dd18306d7
jkwong5 <jkwong5@gmail.com>
2017-07-26 17:46:24 -0700

    add weather

--- server.js
+++ server.js
@@ -47,6 +47,26 @@ app.post("/webhook", function (req, res) {
 
       res.sendStatus(200);
     }
+
+    else if (req.body.result.action === 'weather') {
+      let city = req.body.result.parameters['geo-city'];
+      let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
+
+      request.get(restUrl, (err, response, body) => {
+        if (!err && response.statusCode == 200) {
+          let json = JSON.parse(body);
+          let msg = json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
+          return res.json({
+            speech: msg,
+            displayText: msg,
+            source: 'weather'});
+        } else {
+          return res.status(400).json({
+            status: {
+              code: 400,
+              errorType: 'I failed to look up the city name.'}});
+        }});
+      }
 });
 
 function processPostback(event) {
